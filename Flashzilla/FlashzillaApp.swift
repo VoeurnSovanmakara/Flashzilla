5ea90ac0d2590a220d0474d34a7a53fb21bfe957
itsshinra <voeurnsovanmakara313@gmail.com>
2026-06-12 12:58:45 +0700

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by sovanmakara on 12/6/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlashzillaTests/FlashzillaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaTests.swift
+//  FlashzillaTests
+//
+//  Created by sovanmakara on 12/6/26.
+//
+
+import Testing
+@testable import Flashzilla
+
+struct FlashzillaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
